0f5d093ef8608bfbcb82333cd827a789476ec6fc
Davi-Cunha <davi.silva.1019097@sga.pucminas.br>
2018-05-29 18:17:09 -0300

    Code update

--- index.php
+++ index.php
@@ -2,15 +2,24 @@
 <html lang="en">
 <head>
     <title>Home</title>
-    <?php include("./includes/link-paths-include.php");?>
+    <?php include("./includes/link-paths-include.php"); ?>
+    <link href="./general.css">
 </head>
 <body>
 
-<?php include("./includes/nav-bar-include.php");?>
+<!-- LOGIN OR REGISTER FORM -->
 
-
-
-
+<div class="container">
+    <div class="row">
+        <div class="login-signin-box text-center">
+            <form method="post" action="./homepage/homepage.php">
+                <input type="email" placeholder="E-mail">
+                <input type="password" placeholder="Password">
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
 
 
 </body>
